e7eeef515f08e58cee140b2e0e7c917fce994620
Kevin Tan <ky2tan@uwaterloo.ca>
2026-06-20 20:05:32 -0400

    Added basic image storage/loading

--- app/src/main/java/com/example/wardrobeapp/data/local/LocalImageStore.kt
+++ app/src/main/java/com/example/wardrobeapp/data/local/LocalImageStore.kt
@@ -1,7 +1,10 @@
 package com.example.wardrobeapp.data.local
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
+import java.io.File
 
 /**
  * Helper class for saving and loading clothing photos on the device.
@@ -9,11 +12,22 @@ import android.net.Uri
 class LocalImageStore(private val context: Context) {
     suspend fun saveImage(uri: Uri): String {
         // Implementation for saving image to internal storage
-        return ""
+        // Save image as absolutePath/image_time
+        val filename = "image_${System.currentTimeMillis()}";
+        val destinationFile = File(context.filesDir, filename);
+        return "${context.filesDir.absolutePath}/$filename";
     }
 
-    fun loadImage(path: String): Uri? {
+    fun loadImage(path: String): Bitmap {
         // Implementation for loading image from internal storage
-        return null
+        val imgFile= File(path);
+        var imgBitmap: Bitmap? = null;
+        if (imgFile.exists()){
+            imgBitmap = BitmapFactory.decodeFile(imgFile.absolutePath)
+        }
+        else{
+            throw error("Invalid Path");
+        }
+        return imgBitmap;
     }
 }

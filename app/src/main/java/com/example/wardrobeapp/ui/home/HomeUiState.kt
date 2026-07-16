8ec5d6df66a0920596076f970a2dd1624573ccbb
JohnPei1 <zhengpei.pz@gmail.com>
2026-07-16 15:14:34 -0400

    remove homescreen code

--- app/src/main/java/com/example/wardrobeapp/ui/home/HomeUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.wardrobeapp.ui.home
-
-import com.example.wardrobeapp.domain.model.WeatherInfo
-
-data class HomeUiState(
-    val weather: WeatherInfo? = null,
-    val isLoading: Boolean = false,
-    val error: String? = null
-)
